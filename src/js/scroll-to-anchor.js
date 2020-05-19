a03404593f1400efe8a6ae0a7b6ea7ae67967941
mrToshkin <toshkin.na@gmail.com>
2020-05-20 03:49:47 +0400

    [feature] scroll anchor, color menu current page
    - smooth scroll to anchor
    - event nav menu item color for current page

---
+++ src/js/scroll-to-anchor.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.arrow-anchor').on('click', function(e){
+    $('html,body').stop().animate({ scrollTop: $('#first-main-section').offset().top }, 1000);
+    e.preventDefault();
+  });
+});
