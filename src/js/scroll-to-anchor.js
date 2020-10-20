e101674c7d46d08bb9707a1690866575d6e4610b
mrToshkin <toshkin.na@gmail.com>
2020-10-20 15:22:40 +0400

    gh-pages

--- src/js/scroll-to-anchor.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$(document).ready(function() {
-  $('.arrow-anchor').on('click', function(e){
-    $('html,body').stop().animate({ scrollTop: $('#first-main-section').offset().top }, 1000);
-    e.preventDefault();
-  });
-});
